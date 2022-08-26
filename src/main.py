37a05535abb900f1fd3248f00111253da8e81cc3
fluxink <pavel.volkov.k@gmail.com>
2022-08-26 11:29:06 +0300

    Delete main.py

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import cl.enterprise_python.tests.schema.relational.relational_crud_test
-import cl.enterprise_python.core.api.fastapi.fastapi_blotter
-import cl.enterprise_python.core.api.requests.requests_blotter
-# import pytest
-from pathlib import Path
-
-path = Path(r"D:\WorkSpace\Projects_edu\enterprise-python\src\cl\enterprise_python\tests\schema\relational\relational_crud_test.py")
-
-# pytest.main(path)
